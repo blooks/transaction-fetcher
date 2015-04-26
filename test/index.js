0ca5b0e5128520517d35d4bcef734a846994aac0
Levin Keller <levin@coyno.com>
2015-04-26 14:58:08 +0200

    Simple change.

--- test/index.js
+++ test/index.js
@@ -8,19 +8,19 @@ var debug = require('debug')('coyno:transfers-tests');
 
 describe('Tests for Package Coyno Transfers', function() {
 
-    describe('Unit tests', function () {
+  describe('Unit tests', function () {
+  });
+  describe('Integration tests', function () {
+    describe('Update bitcoin wallet', function () {
+      it('should update all transactions for bitcoin wallet', function (done) {
+        done();
+      });
     });
-    describe('Integration tests', function () {
-            describe('Update bitcoin wallet', function () {
-                it('should update all transactions for bitcoin wallet', function (done) {
-                    done();
-                });
-            });
-            describe('Update and add trades', function () {
-                it('should print a lot of addresses', function (done) {
-                    done(null);
-                });
-            });
-        })
-     });
+    describe('Update and add trades', function () {
+      it('should print a lot of addresses', function (done) {
+        done(null);
+      });
+    });
+  })
+});
 
