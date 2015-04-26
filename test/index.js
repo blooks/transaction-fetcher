af234a755fe1f39a63ce76984c70907af63a6b2a
Levin Keller <levin@coyno.com>
2015-04-26 14:16:04 +0200

    Adding mocha.

--- test/index.js
+++ test/index.js
@@ -1,44 +1,14 @@
 
-var TestDataManager = require('coyno-mockup-data').Manager;
-
 var mongo = require('coyno-mongo');
 var should = require('should');
 var debug = require('debug')('coyno:transfers-tests');
 
-var TransferSet = require('../index');
-
-var testDataManager = new TestDataManager();
 
 describe('Tests for Package Coyno Transfers', function() {
 
     describe('Unit tests', function () {
     });
     describe('Integration tests', function () {
-        before(function (done) {
-            testDataManager.initDB(function (err) {
-                done(err);
-            });
-        });
-        after(function (done) {
-            testDataManager.closeDB(function (err) {
-                if (err) console.log(err);
-                done(err);
-            });
-        });
-        describe('Wallet jobs tests', function() {
-            before(function (done) {
-                testDataManager.fillDB(function (err) {
-                    done(err);
-                });
-            });
-
-            after(function (done) {
-                testDataManager.emptyDB(function (err) {
-                    if (err) console.log(err);
-                    done(err);
-                });
-            });
-
             describe('Update bitcoin wallet', function () {
                 it('should update all transactions for bitcoin wallet', function (done) {
                     done();
@@ -51,5 +21,4 @@ describe('Tests for Package Coyno Transfers', function() {
             });
         })
     });
-});
 
